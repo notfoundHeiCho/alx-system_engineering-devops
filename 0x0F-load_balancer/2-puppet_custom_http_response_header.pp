8205f38bce9bcad96b437035c752a4bafd3d2e9c
ZZZHajar008 <ilyass.infinite345@gmail.com>
2024-01-30 00:16:29 +0100

    Fix & Execute all

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,9 @@
 # instal and configure nginx
 exec {'update':
-    command => '/usr/bin/apt-get update',
+  command => '/usr/bin/apt-get update',
 }
--> package { 'nginx:
-    ensure => installed,
+-> package { 'nginx':
+  ensure => installed,
 }
 -> file_line { 'header_served_by':
   path  => '/etc/nginx/sites-available/default',
